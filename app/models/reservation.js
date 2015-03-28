ab364f97453ce0371bdc6cd10e013af95d22f486
Sally Traynham <sally.traynham@akqa.com>
2015-03-28 16:56:38 -0400

    update schema/api

--- app/models/reservation.js
+++ app/models/reservation.js
@@ -1,8 +1,13 @@
 var mongoose = require('mongoose');
 
 var ReservationSchema = new mongoose.Schema({
-    id: Number,
-    rideId: Number
+    poiId: Number,
+    qty: Number,
+    timestamp: Number,
+    name: String,
+    uid: Number,
+    deviceType: String,
+    deviceId: Number
 });
 
-module.exports = mongoose.model('Reseration', ReservationSchema);
+module.exports = mongoose.model('Reservation', ReservationSchema);
