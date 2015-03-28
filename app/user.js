221d718c9ca19808f8b3fa3d71765e98d6d631de
Sally Traynham <sally.traynham@akqa.com>
2015-03-28 14:45:50 -0400

    initial setup

---
+++ app/user.js
@@ -0,0 +1,7 @@
+var mongoose    = require('mongoose');
+
+var UserSchema  = new mongoose.Schema({
+    id: Number
+});
+
+module.exports  = mongoose.model('User', UserSchema);
---
+++ server.js
@@ -0,0 +1,43 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+
+mongoose.connect('mongodb://harry:harry@ds061208.mongolab.com:61208/hufflepuff');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+var User = require('./app/models/user');
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+router.use(function(req, res, next) {
+	console.log('in the middle');
+	next();
+});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'success!' });
+});
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Started on ' + port);
+
