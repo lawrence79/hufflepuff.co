ebf3635456dfd5db4f79908b0a8b277f16880b8a
Lawrence Norton <lawrence@lawrencenorton.com>
2015-03-29 00:25:59 -0400

    updated

--- server.js
+++ server.js
@@ -49,77 +49,10 @@ router.get('/', function(req, res) {
     res.json({ message: 'success!' });
 });
 
-router.route('/reservations')
-
-    // create a reservation (accessed at POST http://localhost:8080/api/reservations)
-    .post(function(req, res) {
-
-        var reservation = new Reservation();
-
-        reservation.poiId = req.body.poiId;
-        reservation.qty = req.body.qty;
-        reservation.name = req.body.name;
-        reservation.timestamp = req.body.timestamp;
-        reservation.uid = req.body.uid;
-        reservation.deviceType = req.body.deviceType;
-        reservation.deviceId = req.body.deviceId || 0;
-
-        reservation.save(function(err) {
-            if (err) {
-                res.send(err);
-            }
-            res.json({message: 'Reservation created!'});
-        });
-    })
-
-    // get all reservations for all rides (accessed at GET http://localhost:8080/api/reservations)
-    .get(function(req, res) {
-        Reservation.find(function(err, reservations) {
-            if (err)
-                res.send(err);
-
-            res.json(reservations);
-        });
-    });
-
-
-// on routes that end in /reservations/:reservation_id
-router.route('/reservations/:reservation_id')
-
-    // get the reservation with that id (accessed at GET http://localhost:8080/api/reservations/:reservation_id)
-    .get(function(req, res) {
-        Reservation.findById(req.params.reservation_id, function(err, reservation) {
-            if (err)
-                res.send(err);
-            res.json(reservation);
-        });
-    })
-
-    // delete the reservation with this id (accessed at DELETE http://localhost:8080/api/reservations/:reservation_id)
-    .delete(function(req, res) {
-        Reservation.remove({
-            _id: req.params.reservation_id
-        }, function(err, reservation) {
-            if (err)
-                res.send(err);
-            res.json({ message: 'Reservation successfully deleted' });
-        });
-    });
-
-// on routes that end in /reservations/rides/:ride_id
-router.route('/reservations/rides/:ride_id')
-
-    // get the reservation with that id (accessed at GET http://localhost:8080/api/reservations/:reservation_id)
-    .get(function(req, res) {
-        Reservation.find()
-            .where('rideId').equals(req.params.ride_id)
-            .exec(function(err, reservation) {
-
-            if (err)
-                res.send(err);
-            res.json(reservation);
-        });
-    });
+router.post('/play',function(req, res) {
+    res.json(req.body);
+    console.log(req.body);
+});
 
 var apiController = require('./app/routes/api');
 
